1336bff57d27e29ec769ed53c48104454608dd40
dacandido <diegoapcandido@gmail.com>
2023-04-25 15:37:39 -0300

    testando atualizacao no servidor commit 2

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
 const app = express();
+const port = process.env.PORT || 3002
+
+const produtos = require("./src/produtos/produtos.json")
 
 app.get('/',(request,response)=>{
     return response.json({message:'Server is up'});
@@ -10,9 +13,17 @@ app.get('/atualizou',(request,response)=>{
     return response.json({message:'Commit atualizou'});
 })
 
+app.get('/produtos',(req,res)=>{
+    return res.json(produtos);
+})
+
 app.post('/teste',(request,response)=>{
     const {name,date} = request.body;
     return response.json({name,date});
 })
 
-app.listen(3002)
+
+
+app.listen(port, () => {
+    console.log("Servidor está rodando ... aws")
+})
